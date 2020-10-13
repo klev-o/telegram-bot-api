a7a5cf10b32a61380dc0764c3ae9f3d5fe080222
Konstantin <levachin@luckandroll.com>
2020-10-13 22:10:43 +0300

    Changed Animation and Audio

--- src/Types/Animation.php
+++ src/Types/Animation.php
@@ -13,38 +13,48 @@ namespace Klev\TelegramBotApi\Types;
 class Animation extends BaseType
 {
     /**
+     * Identifier for this file, which can be used to download or reuse the file
      * @var string
      */
     public string $file_id;
     /**
+     * Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be
+     * used to download or reuse the file.
      * @var string
      */
     public string $file_unique_id;
     /**
+     * Video width as defined by sender
      * @var int
      */
     public int $width;
     /**
+     * Video height as defined by sender
      * @var int
      */
     public int $height;
     /**
+     * Duration of the video in seconds as defined by sender
      * @var int
      */
     public int $duration;
     /**
+     * Optional. Animation thumbnail as defined by sender
      * @var PhotoSize|null
      */
     public ?PhotoSize $thumb;
     /**
+     * Optional. Original animation filename as defined by sender
      * @var string|null
      */
     public ?string $file_name;
     /**
+     * Optional. MIME type of the file as defined by sender
      * @var string|null
      */
     public ?string $mime_type;
     /**
+     * Optional. File size
      * @var int|null
      */
     public ?int $file_size;
--- src/Types/Audio.php
+++ src/Types/Audio.php
@@ -13,35 +13,54 @@ namespace Klev\TelegramBotApi\Types;
 class Audio extends BaseType
 {
     /**
+     * Identifier for this file, which can be used to download or reuse the file
      * @var string
      */
     public string $file_id;
     /**
+     * Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be
+     * used to download or reuse the file.
      * @var string
      */
     public string $file_unique_id;
     /**
+     * Duration of the audio in seconds as defined by sender
      * @var int
      */
     public int $duration;
     /**
+     * Optional. Performer of the audio as defined by sender or by audio tags
      * @var string|null
      */
     public ?string $performer;
     /**
+     * Optional. Title of the audio as defined by sender or by audio tags
      * @var string|null
      */
     public ?string $title;
     /**
+     * Optional. MIME type of the file as defined by sender
      * @var string|null
      */
     public ?string $mime_type;
     /**
+     * Optional. File size
      * @var int|null
      */
     public ?int $file_size;
     /**
+     * Optional. Thumbnail of the album cover to which the music file belongs
      * @var PhotoSize|null
      */
     public ?PhotoSize $thumb;
+
+    protected function bindObjects($key, $data): ?object
+    {
+        switch ($key) {
+            case 'thumb':
+                return new PhotoSize($data);
+        }
+
+        return null;
+    }
 }
