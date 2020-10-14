32f6da51a9d468455a93598ff9062c46d32d0196
Konstantin <levachin@luckandroll.com>
2020-10-14 09:23:13 +0300

    Added ForceReply

---
+++ src/Types/ForceReply.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Klev\TelegramBotApi\Types;
+
+/**
+ * Upon receiving a message with this object, Telegram clients will display a reply interface to the user (act as if
+ * the user has selected the bot's message and tapped 'Reply'). This can be extremely useful if you want to create
+ * user-friendly step-by-step interfaces without having to sacrifice privacy mode.
+ *
+ * Class ForceReply
+ * @package Klev\TelegramBotApi\Types
+ *
+ * @see https://core.telegram.org/bots/api#forcereply
+ */
+class ForceReply extends BaseType
+{
+    /**
+     * Shows reply interface to the user, as if they manually selected the bot's message and tapped 'Reply'
+     * @var bool
+     */
+    public bool $force_reply = true;
+    /**
+     * Optional. Use this parameter if you want to force reply from specific users only. Targets:
+     * 1) users that are @mentioned in the text of the Message object;
+     * 2) if the bot's message is a reply (has reply_to_message_id), sender of the original message.
+     * @var bool|null
+     */
+    public ?bool $selective;
+}
