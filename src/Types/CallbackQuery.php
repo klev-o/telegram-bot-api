b8731cb8de6b10ea432091d1dbdaafe084c1286c
Konstantin <levachin@luckandroll.com>
2020-11-06 09:33:25 +0300

    Changed CallbackQuery

--- src/Types/CallbackQuery.php
+++ src/Types/CallbackQuery.php
@@ -3,6 +3,11 @@
 namespace Klev\TelegramBotApi\Types;
 
 /**
+ * This object represents an incoming callback query from a callback button in an inline keyboard. If the button that
+ * originated the query was attached to a message sent by the bot, the field message will be present. If the button was
+ * attached to a message sent via the bot (in inline mode), the field inline_message_id will be present. Exactly one of
+ * the fields data or game_short_name will be present.
+ *
  * Class CallbackQuery
  * @package Klev\TelegramBotApi\Types
  *
@@ -49,6 +54,11 @@ class CallbackQuery extends BaseType
      */
     public ?string $game_short_name;
 
+    /**
+     * @param $key
+     * @param $data
+     * @return object|null
+     */
     protected function bindObjects($key, $data): ?object
     {
         switch ($key) {
