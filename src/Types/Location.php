c238abef2117a508e78cca8727becf18712b70b5
Konstantin <levachin@luckandroll.com>
2020-11-06 09:23:38 +0300

    Changed Location

--- src/Types/Location.php
+++ src/Types/Location.php
@@ -22,4 +22,26 @@ class Location extends BaseType
      * @var float
      */
     public float $latitude;
+    /**
+     * Optional. The radius of uncertainty for the location, measured in meters; 0-1500
+     * @var float|null
+     */
+    public ?float $horizontal_accuracy;
+    /**
+     * Optional. Time relative to the message sending date, during which the location can be updated, in seconds.
+     * For active live locations only.
+     * @var int|null
+     */
+    public ?int $live_period;
+    /**
+     * Optional. The direction in which user is moving, in degrees; 1-360. For active live locations only.
+     * @var int|null
+     */
+    public ?int $heading;
+    /**
+     * Optional. Maximum distance for proximity alerts about approaching another chat member, in meters. For sent live
+     * locations only.
+     * @var int|null
+     */
+    public ?int $proximity_alert_radius;
 }
