cd28f5017f4346df7b49c7f2eb6570f00e4860de
Konstantin <levachin@luckandroll.com>
2020-11-06 09:47:52 +0300

    Added InputMediaPhoto

---
+++ src/Types/InputMediaPhoto.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Klev\TelegramBotApi\Types;
+
+/**
+ * Represents a photo to be sent.
+ *
+ * Class InputMediaPhoto
+ * @package Klev\TelegramBotApi\Types
+ *
+ * @see https://core.telegram.org/bots/api#inputmediaphoto
+ */
+class InputMediaPhoto implements InputMedia
+{
+    /**
+     * Type of the result, must be photo
+     * @var string
+     */
+    public string $type = 'photo';
+    /**
+     * File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), pass an HTTP
+     * URL for Telegram to get a file from the Internet, or pass “attach://<file_attach_name>” to upload a new one
+     * using multipart/form-data under <file_attach_name> name. More info on Sending Files »
+     * @var string
+     */
+    public string $media;
+    /**
+     * Optional. Caption of the photo to be sent, 0-1024 characters after entities parsing
+     * @var string|null
+     */
+    public ?string $caption = '';
+    /**
+     * Optional. Mode for parsing entities in the photo caption. See formatting options for more details.
+     * @var string|null
+     */
+    public ?string $parse_mode = 'html';
+    /**
+     * Optional. List of special entities that appear in the caption, which can be specified instead of parse_mode
+     * @var array|null
+     */
+    public ?array $caption_entities;
+
+
+    public function __construct(string $media) //todo
+    {
+        $this->media = $media;
+    }
+
+    /**
+     * @param mixed $media
+     */
+    public function setMedia($media): void
+    {
+        $this->media = $media;
+    }
+
+    public function getMedia()
+    {
+        return $this->media;
+    }
+
+    /**
+     * @param string $caption
+     */
+    public function setCaption(string $caption): void
+    {
+        $this->caption = $caption;
+    }
+
+    /**
+     * @param string $parse_mode
+     */
+    public function setParseMode(string $parse_mode): void
+    {
+        $this->parse_mode = $parse_mode;
+    }
+}
