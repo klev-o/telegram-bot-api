7f4ae1679d29f3c31b12780306b38efdb4d52a9b
Konstantin <levachin@luckandroll.com>
2020-11-06 09:38:28 +0300

    Added BotCommand

---
+++ src/Types/BotCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Klev\TelegramBotApi\Types;
+
+/**
+ * This object represents a bot command.
+ *
+ * Class BotCommand
+ * @package Klev\TelegramBotApi\Types
+ *
+ * @see https://core.telegram.org/bots/api#botcommand
+ */
+class BotCommand extends BaseType
+{
+    /**
+     * Text of the command, 1-32 characters. Can contain only lowercase English letters, digits and underscores.
+     * @var string
+     */
+    public string $command;
+    /**
+     * Description of the command, 3-256 characters.
+     * @var string
+     */
+    public string $description;
+}
