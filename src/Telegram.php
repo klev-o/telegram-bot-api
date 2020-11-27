34e537ec84281ff446ac7b53ec9de224ce35221c
Konstantin <levachin@luckandroll.com>
2020-11-27 22:22:16 +0300

    exportChatInviteLink

--- src/Telegram.php
+++ src/Telegram.php
@@ -537,6 +537,22 @@ class Telegram
         return $out['result'];
     }
 
+    /**
+     * Use this method to generate a new invite link for a chat; any previously generated link is revoked. The bot must
+     * be an administrator in the chat for this to work and must have the appropriate admin rights. Returns the new
+     * invite link as String on success.
+     *
+     * @param string $chat_id
+     * @return mixed
+     * @throws GuzzleException
+     * @throws TelegramException
+     */
+    public function exportChatInviteLink(string $chat_id): string
+    {
+        $out = $this->request('exportChatInviteLink', ['json' => ['chat_id' => $chat_id]]);
+        return $out['result'];
+    }
+
 
 
 
