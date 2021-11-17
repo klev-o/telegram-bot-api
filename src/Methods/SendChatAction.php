662a386b14c3e80fc07f70fed0239e372e14c3e7
Konstantin <levachin@luckandroll.com>
2021-11-17 17:26:50 +0300

    Added support for the choose_sticker action in the method sendChatAction.

--- src/Methods/SendChatAction.php
+++ src/Methods/SendChatAction.php
@@ -22,8 +22,8 @@ class SendChatAction extends BaseMethod
     /**
      * Type of action to broadcast. Choose one, depending on what the user is about to receive: typing for text
      * messages, upload_photo for photos, record_video or upload_video for videos, record_voice or upload_voice for
-     * voice notes, upload_document for general files, find_location for location data, record_video_note or
-     * upload_video_note for video notes.
+     * voice notes, upload_document for general files, choose_sticker for stickers find_location for location data,
+     * record_video_note or upload_video_note for video notes.
      * @var string
      */
     public string $action;
