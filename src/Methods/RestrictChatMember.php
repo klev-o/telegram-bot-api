881f3ab7c530e1af316610d90379deeb27c531c9
KonstantinLev <x-stels@bk.ru>
2023-02-05 17:29:59 +0300

    Added the parameter use_independent_chat_permissions to the methods restrictChatMember and setChatPermissions.

--- src/Methods/RestrictChatMember.php
+++ src/Methods/RestrictChatMember.php
@@ -33,6 +33,14 @@ class RestrictChatMember extends BaseMethod
      */
     public $permissions = '';
     /**
+     * Pass True if chat permissions are set independently. Otherwise, the can_send_other_messages and
+     * can_add_web_page_previews permissions will imply the can_send_messages, can_send_audios, can_send_documents,
+     * can_send_photos, can_send_videos, can_send_video_notes, and can_send_voice_notes permissions; the can_send_polls
+     * permission will imply the can_send_messages permission
+     * @var bool|null
+     */
+    public ?bool $use_independent_chat_permissions = null;
+    /**
      * Date when restrictions will be lifted for the user, unix time. If user is restricted for more than 366 days or
      * less than 30 seconds from the current time, they are considered to be restricted forever
      * @var int|null
--- src/Methods/SetChatPermissions.php
+++ src/Methods/SetChatPermissions.php
@@ -25,6 +25,14 @@ class SetChatPermissions extends BaseMethod
      * @var ChatPermissions
      */
     public $permissions = '';
+    /**
+     * Pass True if chat permissions are set independently. Otherwise, the can_send_other_messages and
+     * can_add_web_page_previews permissions will imply the can_send_messages, can_send_audios, can_send_documents,
+     * can_send_photos, can_send_videos, can_send_video_notes, and can_send_voice_notes permissions; the can_send_polls
+     * permission will imply the can_send_messages permission.
+     * @var bool|null
+     */
+    public ?bool $use_independent_chat_permissions = null;
 
     public function  __construct($chat_id, ChatPermissions $permissions)
     {
