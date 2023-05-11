9a3da3df24682dda97454f0ccd05ac133369e302
KonstantinLev <x-stels@bk.ru>
2023-05-11 04:43:17 +0300

    Added caption field, renamed 'entities' field

--- src/Methods/UpdatingMessages/EditMessageCaption.php
+++ src/Methods/UpdatingMessages/EditMessageCaption.php
@@ -36,20 +36,32 @@ class EditMessageCaption extends BaseMethod
      */
     public ?string $inline_message_id = null;
     /**
+     * TODO remove in future versions
      * New caption of the message, 0-1024 characters after entities parsing
      * @var string|null
      */
     public ?string $text = '';
+    /**
+     * New caption of the message, 0-1024 characters after entities parsing
+     * @var string|null
+     */
+    public ?string $caption = '';
     /**
      * Mode for parsing entities in the message text. See formatting options for more details.
      * @var string
      */
     public string $parse_mode = 'html';
     /**
+     * TODO remove in future versions
      * List of special entities that appear in message text, which can be specified instead of parse_mode
      * @var MessageEntity[]|null
      */
     public ?array $entities = null;
+    /**
+     * List of special entities that appear in message text, which can be specified instead of parse_mode
+     * @var MessageEntity[]|null
+     */
+    public ?array $caption_entities = null;
     /**
      * Disables link previews for links in this message
      * @var bool
