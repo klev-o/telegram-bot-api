1582a33adaed165b5804869b19cc2352031f30a7
KonstantinLev <x-stels@bk.ru>
2024-12-03 15:59:14 +0300

    Added the parameter business_connection_id to the method sendChatAction.

--- src/Methods/SendChatAction.php
+++ src/Methods/SendChatAction.php
@@ -15,6 +15,11 @@ namespace Klev\TelegramBotApi\Methods;
 class SendChatAction extends BaseMethod
 {
     /**
+     * Unique identifier of the business connection on behalf of which the message will be sent
+     * @var string|null
+     */
+    public ?string $business_connection_id = null;
+    /**
      * Unique identifier for the target chat or username of the target channel (in the format @channelusername)
      * @var string
      */
