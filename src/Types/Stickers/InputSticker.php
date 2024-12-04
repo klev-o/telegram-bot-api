554429196aaa8c6d985ae49648448443dfce7711
KonstantinLev <x-stels@bk.ru>
2024-12-04 14:05:23 +0300

    Added the field format to the class InputSticker.

--- src/Types/Stickers/InputSticker.php
+++ src/Types/Stickers/InputSticker.php
@@ -23,6 +23,12 @@ class InputSticker extends BaseType
      */
     public string $sticker;
     /**
+     * Format of the added sticker, must be one of “static” for a .WEBP or .PNG image, “animated” for a .TGS animation,
+     * “video” for a WEBM video
+     * @var string
+     */
+    public string $format;
+    /**
      * List of 1-20 emoji associated with the sticker
      * @var string[]
      */
