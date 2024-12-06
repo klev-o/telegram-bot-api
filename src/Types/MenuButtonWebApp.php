a96f7124ef57e83e5f8a0b926f484c4700e633cb
KonstantinLev <x-stels@bk.ru>
2024-12-06 11:29:03 +0300

    Added support for launching Web Apps via t.me link in the class MenuButtonWebApp.

--- src/Types/MenuButtonWebApp.php
+++ src/Types/MenuButtonWebApp.php
@@ -23,8 +23,10 @@ class MenuButtonWebApp extends MenuButton
      */
     public string $text;
     /**
-     * 	Description of the Web App that will be launched when the user presses the button. The Web App will be able to
-     * send an arbitrary message on behalf of the user using the method answerWebAppQuery.
+     * Description of the Web App that will be launched when the user presses the button. The Web App will be able to
+     * send an arbitrary message on behalf of the user using the method answerWebAppQuery. Alternatively, a t.me link
+     * to a Web App of the bot can be specified in the object instead of the Web App's URL, in which case the Web App
+     * will be opened as if the user pressed the link.
      * @var WebAppInfo
      */
     public WebAppInfo $web_app;
